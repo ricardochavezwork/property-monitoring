eaf103c1d0fe2b54c4e5d79356dc61e274c42641
Ricardo Chavez <ricardo.chavez.work@gmail.com>
2020-02-27 16:28:40 +0100

    Goutte test
    This is a test usage of Goutte

--- public/test.php
+++ public/test.php
@@ -1,7 +1,22 @@
 <?php 
 
-/*Hello world*/
-/* Test 2 */
-$var = "Hello world";
+$goutte_dir = "../vendor/Goutte-master/goutte-v1.0.7.phar";
 
-echo $var;
+if(file_exists($goutte_dir)){
+  include_once $goutte_dir;
+}
+
+use Goutte\Client;
+$client = new Client();
+
+// Go to the symfony.com website
+$crawler = $client->request('GET', 'http://www.symfony.com/blog/');
+
+// Click on the "Security Advisories" link
+$link = $crawler->selectLink('Security Advisories')->link();
+$crawler = $client->click($link);
+
+// Get the latest post in this category and display the titles
+$crawler->filter('h2 > a')->each(function ($node) {
+  print $node->text()."\n";
+});
